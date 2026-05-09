8ab05988796f32a1f14b5075572e99b945d2c93a
tariqmiqdad <tariqze222@gmail.com>
2026-05-09 17:56:44 +0300

    Final updates

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    $name = 'Tariq Miqdad';
+    $departments = [
+        '1'=>'Tichnical',
+        '2'=>'Financial',
+        '3'=>'Sales',
+    ];
+    // return view('about')->with('name' , $name);
+    // return view('about' , ['name' => $name]);
+    return view('about', compact('name' , 'departments'));
+});
+
+Route::post('/about', function () {
+    $name = $_POST['name'];
+    $department = [
+        '1'=>'Tichnical',
+        '2'=>'Financial',
+        '3'=>'Sales',
+    ];
+    return view('about' , compact('name'));
+});
